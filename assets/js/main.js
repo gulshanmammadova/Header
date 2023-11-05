cb902527e6f042aefae467fb735930015a0be174
gulshanmammadova <gulshanfm@code.edu.az>
2023-11-05 05:50:01 -0800

    last

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,26 @@
-$('.responsive').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
+$(document).ready(function(){
+  $('.slider').slick({
+    infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 4, 
+     autoplay: true,
+     arrows:false,
+    autoplaySpeed: 3000,
     responsive: [
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
+        breakpoint: 590,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
-        }
+          slidesToScroll: 1,
+        },
       },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
+    ],
   });
-      
+  
+  $('.single-item').slick({
+    autoplay: true,
+    arrows:false,
+    infinite: true,
+    autoplaySpeed: 2000,
+  });
+});
